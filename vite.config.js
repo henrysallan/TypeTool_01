30c0b8a7924ad00e1238a3cad58d01c4ecea5c50
henrysallan <isthishenry@gmail.com>
2025-07-21 00:52:11 -0400

    feat: Add GitHub Actions workflow for automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Typetool_01/',
   plugins: [react()],
   server: { host: true },
 })
