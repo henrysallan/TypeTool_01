49ca9781c77b7fbf545c9c14b9983676c46620ba
henrysallan <isthishenry@gmail.com>
2025-07-21 00:57:34 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Typetool_01/',
+  base: '/TypeTool_01/',
   plugins: [react()],
   server: { host: true },
 })
